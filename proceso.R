265305a0c512a7631dd917ac458458544ac71840
Luis M Silva <lmsilvam@zoho.com>
2022-06-06 10:46:48 -0500

    Cambios para estandarizar

--- proceso.R
+++ proceso.R
@@ -1,7 +1,10 @@
 library(tidyverse)
+library(stringi)
 library(kable)
 
 data <- read_csv("data.csv")
+citescore <- read_delim("citescore.csv", 
+                        delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 # Construir tabla de autores a partir del campo Author
 autores <- data %>% 
@@ -9,7 +12,19 @@ autores <- data %>%
   separate_rows(Author, sep=';') %>% 
   unique()
 
-revistas <- data %>%
-  arrange('Publication Title') %>% 
-  group_by('Publication Title') %>% 
-  tally() 
+revistas <- data %>% 
+  filter(`Item Type` == "journalArticle") %>% 
+  rename('PubTitle' = `Publication Title`) %>% 
+  mutate(PubTitle = stri_trans_general(str=str_replace_all(PubTitle, ',', ';'), id="Latin-ASCII")) %>% 
+  group_by(PubTitle) %>% 
+  tally() %>% 
+  arrange(PubTitle)
+
+scopus <- citescore %>% 
+  rename('Area' = `Scopus Sub-Subject Area`) %>% 
+  select(Title, Area, Quartile) %>% 
+  distinct(Title, .keep_all = T)
+
+total=merge(revistas, scopus, by.x='PubTitle', by.y='Title', all.x = T)
+soloind=merge(revistas, scopus, by.x='PubTitle', by.y='Title')
+
