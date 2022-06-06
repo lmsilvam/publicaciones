bb41965b075bb81496d158eece67dfb747f9ae5a
lmsilvam <lmsilvam@users.noreply.github.com>
2022-06-06 05:25:30 -0500

    Primeras tablas

--- proceso.R
+++ proceso.R
@@ -4,3 +4,12 @@ library(kable)
 data <- read_csv("data.csv")
 
 # Construir tabla de autores a partir del campo Author
+autores <- data %>% 
+  select(Author) %>% 
+  separate_rows(Author, sep=';') %>% 
+  unique()
+
+revistas <- data %>%
+  arrange('Publication Title') %>% 
+  group_by('Publication Title') %>% 
+  tally() 
